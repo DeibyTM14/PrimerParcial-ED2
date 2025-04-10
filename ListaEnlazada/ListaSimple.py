bc65ee622a8570f6c8f16ebc38f328719ecc03e1
¨Alexis¨ <diegoalexis355@gmail.com>
2025-04-09 20:50:43 -0400

    metodo insertar_final y desapilar

--- ListaEnlazada/ListaSimple.py
+++ ListaEnlazada/ListaSimple.py
@@ -22,14 +22,16 @@ class ListaSimple:
 
 ############PREGUNTA 1: Método insertar_final ###########
     # Método para insertar un nodo al final de la lista
-
-
-
-
-
-
-
-
+    def insertar_final(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if self.esta_vacia():
+            self.cabeza = nuevo_nodo
+        else:
+            actual = self.cabeza
+            while actual.siguiente is not None:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+            self.longitud += 1
 
 ########################################################
     def eliminar_inicio(self):
@@ -40,7 +42,7 @@ class ListaSimple:
         self.cabeza = self.cabeza.siguiente
         self.longitud -= 1
         return valor
-    
+
     def eliminar_final(self):
         if self.esta_vacia():
             raise ValueError("No se puede eliminar de una lista vacía")
